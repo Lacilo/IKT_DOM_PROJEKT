67734dd010c0e034b9425992195ebc29d6346115
lacilo <szaszfail@kkszki.hu>
2026-05-10 10:08:35 +0200

    ellenőrző függvény létrehozása

--- dobokocka_parbaj/index.js
+++ dobokocka_parbaj/index.js
@@ -4,8 +4,19 @@ class Dobokocka {
     }
 
     chageValue = (newValue = 0) => {
-        if (newValue == 0){
+        if (newValue == 0) {
             this.value = Math.floor(Math.random() * 6) + 1;
         }
     }
 }
+
+const getLargerCube = (c1, c2) => {
+    if(c1.value > c2.value) {
+        return c1;
+    }else if(c2.value > c1.value) {
+        return c2;
+    }else {
+        return null;
+    }
+}
+
