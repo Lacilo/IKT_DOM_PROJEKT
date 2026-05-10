c29474a9555a209fc3853b74c337046f0dddf401
lacilo <szaszfail@kkszki.hu>
2026-05-10 11:33:09 +0200

    .

--- ki_nevet_a_vegen/index.js
+++ ki_nevet_a_vegen/index.js
@@ -1,11 +1,36 @@
 const box = document.getElementById("box");
 const time = document.getElementById("time");
 
+const roundEnd = (condition) => {
+    if(condition === "win"){
+        box.style.backgroundColor = "orange";
+    }else{
+
+    }
+}
+
 const startRound = () => {
     box.style.backgroundColor = "red";
     time.innerHTML = "";
+
+    changeTime = (Math.random() * 6) + 2;
+    console.log(changeTime);
+
+    setTimeout(() => {
+        box.style.backgroundColor = "green";
+    }, changeTime * 1000);
 }
 
 box.onclick = () => {
-    box.style.backgroundColor = "green";
+    if(box.style.backgroundColor === "green"){
+        roundEnd("win");
+    }else{
+        roundEnd("lose");
+    }
+}
+
+box.onmouseover = () => {
+    box.style.backgroundColor = "red";
+    time.innerHTML = 0;
+    startRound();
 }
